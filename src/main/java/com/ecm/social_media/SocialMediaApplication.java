ff37ed1b56d2efc150e8ef5e87a0f0f66e793951
rodapunk <estevanmachry@gmail.com>
2024-01-22 22:09:14 -0300

    First commit - Project created

---
+++ src/main/java/com/ecm/social_media/SocialMediaApplication.java
@@ -0,0 +1,11 @@
+package com.ecm.social_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ecm/social_media/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecm.social_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
