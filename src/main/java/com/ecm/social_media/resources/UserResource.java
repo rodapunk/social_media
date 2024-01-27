7c9beaa255cbdd16cec1f95452c5d8535029695b
rodapunk <estevanmachry@gmail.com>
2024-01-27 19:26:14 -0300

    Using DTO pattern to retrieve users

--- src/main/java/com/ecm/social_media/resources/UserResource.java
+++ src/main/java/com/ecm/social_media/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ecm.social_media.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecm.social_media.domain.User;
+import com.ecm.social_media.dto.UserDTO;
 import com.ecm.social_media.services.UserService;
 
 @RestController
@@ -18,8 +20,9 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
