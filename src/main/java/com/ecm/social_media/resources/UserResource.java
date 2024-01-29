433c7f11bc065ebe1f4c4282b423a754b2a12f29
rodapunk <estevanmachry@gmail.com>
2024-01-29 19:49:44 -0300

    Endpoint to retrieve a user's posts

--- src/main/java/com/ecm/social_media/resources/UserResource.java
+++ src/main/java/com/ecm/social_media/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ecm.social_media.domain.Post;
 import com.ecm.social_media.domain.User;
 import com.ecm.social_media.dto.UserDTO;
 import com.ecm.social_media.services.UserService;
@@ -59,5 +60,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findByPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
