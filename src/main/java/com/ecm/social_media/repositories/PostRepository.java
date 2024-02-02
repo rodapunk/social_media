6c863727969dad3fdba17eaf4fd8d32d606a06e4
rodapunk <estevanmachry@gmail.com>
2024-02-02 15:23:15 -0300

    Simple query with @Query

--- src/main/java/com/ecm/social_media/repositories/PostRepository.java
+++ src/main/java/com/ecm/social_media/repositories/PostRepository.java
@@ -3,9 +3,13 @@ package com.ecm.social_media.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.ecm.social_media.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
